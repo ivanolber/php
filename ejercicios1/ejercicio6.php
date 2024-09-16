f78c8ff48e576a60cf2e45356bc5f5620d451b48
Ivan Olmos <ivan.ol.ber@gmail.com>
2024-09-16 20:24:04 +0200

    ejercicios de notas

---
+++ ejercicios1/ejercicio6.php
@@ -0,0 +1,18 @@
+<?php
+
+$nota1 = 7;
+$nota2 = 5;
+$nota3 = 4;
+
+if ($nota1 > $nota2){
+
+    echo("La nota 1 es mayor que la nota 2");
+
+
+}else {
+
+    echo("La nota 2 es mayor que la nota 1");
+
+}
+
+?>
---
+++ ejercicios1/prueba_if.php
@@ -0,0 +1,19 @@
+<?php
+
+$nota1 = 7;
+$nota2 = 5;
+
+if ($nota1 > $nota2){
+
+    echo("La nota 1 es mayor que la nota 2");
+
+
+}else {
+
+    echo("La nota 2 es mayor que la nota 1");
+
+}
+
+?>
+
+
