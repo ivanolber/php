f1860a925c3aec0c0c8c74ef9d21ab0d9d24629f
Ivan Olmos <ivan.ol.ber@gmail.com>
2024-09-16 21:00:54 +0200

    Add files via upload

---
+++ ejercicios1/contador.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Conteo de 1 a 100:<br>";
+for ($i = 1; $i <= 100; $i++) {
+    echo $i;
+    if ($i < 100) {
+        echo ","; 
+    }
+}
+
+$j = 10;
+
+echo "<br>Conteo del 10 al 0<br>";
+while ($j >= 0) {
+    echo $j;
+    if ($j > 0) {
+        echo "-"; 
+    }
+    $j--;
+}
+
+?>
