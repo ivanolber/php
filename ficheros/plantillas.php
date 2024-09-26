2554c4c947b195ca5fd79e051cc8d1fa897f3453
Ivan Olmos <ivan.ol.ber@gmail.com>
2024-09-26 18:57:16 +0200

    Add files via upload

--- ficheros/plantillas.php
+++ ficheros/plantillas.php
@@ -1,1 +1,30 @@
+<?php
 
+$fichero = "plantillas.csv";
+$atleticoData = [];
+
+if (($fich = fopen($fichero, 'r')) !== false) {
+    $cabecera = fgetcsv($fich, 1000, ',');
+
+    while (($fila = fgetcsv($fich, 1000, ',')) !== false) {
+        if ($fila[1] == "Atlético de Madrid") {
+            $atleticoData[] = [
+                'Dorsal' => $fila[11],
+                'Nombre' => $fila[4],
+                'Apellidos' => $fila[5],
+                'Posición' => $fila[9],
+                'Equipo' => $fila[1],
+            ];
+        }
+    }
+    fclose($fich);
+} else {
+    echo "Error al abrir el archivo.";
+}
+
+usort($atleticoData, function($a, $b){
+    return $a['Dorsal'] - $b['Dorsal'];
+});
+
+include 'plantillas_view.php';
+?>
