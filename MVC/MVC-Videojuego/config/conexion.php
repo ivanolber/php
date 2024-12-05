22a976ee77daa74d21cee706f79ed893e8d66e82
Ivan Olmos <ivan.ol.ber@gmail.com>
2024-12-05 08:45:49 +0100

    Create conexion.php

---
+++ MVC/MVC-Videojuego/config/conexion.php
@@ -0,0 +1,31 @@
+<?php
+
+class Conexion {
+    private $pdo;
+    private $host;
+    private $nombreBD ;
+    private $usuario;
+    private $password ;
+    public function __construct()
+    {
+    $this->pdo="";
+    $this->host = "localhost";
+    $this->nombreBD = "videojuego";
+    $this->usuario = "root";
+    $this->password = "";
+    }
+    public function conectar(){
+
+    try{
+    $this->pdo = new PDO("mysql:host=$this->host;dbname=$this->nombreBD;charset=utf8",$this->usuario, $this->password);
+    return $this->pdo;
+        }   catch (PDOException $e) {
+        print " <p class=\"aviso\">Error: No puede conectarse con
+        la base de datos. {$e->getMessage()}</p>\n";
+            exit;
+}
+}
+}
+
+
+?>
