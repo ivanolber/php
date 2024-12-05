f36f81994519305da80399e7c28999e0b3c19e5d
Ivan Olmos <ivan.ol.ber@gmail.com>
2024-12-05 16:11:37 +0100

    Add files via upload

---
+++ MVC/MVC-Videojuego/config/configLogin.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'usuarios';
+$username = 'root';
+$password = '';
+
+try {
+    $conexion = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error al conectar con la base de datos: " . $e->getMessage());
+}
+?>
